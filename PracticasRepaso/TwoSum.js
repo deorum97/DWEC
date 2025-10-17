51c1d653ebb4e27fab8f342685912839dec18ff0
deo <jorge.r.9763@gmail.com>
2025-10-17 14:15:01 +0200

    cambios en ejercicios de repaso

--- PracticasRepaso/TwoSum.js
+++ PracticasRepaso/TwoSum.js
@@ -10,8 +10,12 @@ Encontrar los índices i y j donde nums[i] + nums[j] === target.
 
 function TwoSum(nums, target) {
   for (i = 0; i < nums.length; i++) {
-    for(j=(i+1);j<nums.length;j++){
-        if(nums[i])
+    for (j = i + 1; j < nums.length; j++) {
+      if (nums[i] !== nums[j]) {
+        if (nums[i] + nums[j] === target) {
+          console.log(nums[i] + " " + nums[j] + " " + target);
+        }
+      }
     }
   }
 }
