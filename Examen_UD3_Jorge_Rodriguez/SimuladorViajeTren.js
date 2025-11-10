537645b6581e12d904a2960c7cac1770a4426bab
Vespertino <Vespertino@C04PC07.informatica.comercio>
2025-11-10 17:28:04 +0100

    Examen T3

---
+++ Examen_UD3_Jorge_Rodriguez/SimuladorViajeTren.js
@@ -0,0 +1,67 @@
+"use strict";
+
+import {
+  SODOR_TO_KNAPFORD,
+  SODOR_TO_VICARSTOWN,
+  KANPFORD_TO_VICARSTOWN,
+  VICARSTOWN_TO_TIDMOUTH,
+} from "./constants.js";
+
+export function viajeTren(origenTren, destinoTren) {
+  switch (origenTren) {
+    case "sodor":
+      if (destinoTren === "knapford") {
+        return SODOR_TO_KNAPFORD;
+      } else if (destinoTren === "vicarstown") {
+        return SODOR_TO_VICARSTOWN;
+      } else if (destinoTren === "tidmouth") {
+        return SODOR_TO_VICARSTOWN + KANPFORD_TO_VICARSTOWN;
+      } else {
+        return 0;
+      }
+    case "knapford":
+      if (destinoTren === "sodor") {
+        return SODOR_TO_KNAPFORD;
+      } else if (destinoTren === "vicarstown") {
+        return KANPFORD_TO_VICARSTOWN;
+      } else if (destinoTren === "tidmouth") {
+        return VICARSTOWN_TO_TIDMOUTH + KANPFORD_TO_VICARSTOWN;
+      } else {
+        return 0;
+      }
+    case "vicarstown":
+      if (destinoTren === "sodor") {
+        return SODOR_TO_VICARSTOWN;
+      } else if (destinoTren === "knapford") {
+        return KANPFORD_TO_VICARSTOWN;
+      } else if (destinoTren === "tidmouth") {
+        return VICARSTOWN_TO_TIDMOUTH;
+      } else {
+        return 0;
+      }
+    case "tidmouth":
+      if (destinoTren === "sodor") {
+        return VICARSTOWN_TO_TIDMOUTH + SODOR_TO_VICARSTOWN;
+      } else if (destinoTren === "knapford") {
+        return VICARSTOWN_TO_TIDMOUTH + SODOR_TO_KNAPFORD;
+      } else if (destinoTren === "vicarstown") {
+        return VICARSTOWN_TO_TIDMOUTH;
+      } else {
+        return 0;
+      }
+    default:
+      alert("elige un tren");
+  }
+}
+
+export function eventoViaje() {
+  const evento = Math.floor(Math.random() * 3);
+  switch (evento) {
+    case 0:
+      return "¡Retraso por ovejas! (+10 min)";
+    case 1:
+      return "¡Retraso por averia! (+30 min)";
+    case 2:
+      return "Viaje sin incidentes";
+  }
+}
---
+++ Examen_UD3_Jorge_Rodriguez/constants.js
@@ -0,0 +1,5 @@
+export const HISTORIC_KEY = "tren_historico";
+export const SODOR_TO_KNAPFORD = 25;
+export const SODOR_TO_VICARSTOWN = 25;
+export const KANPFORD_TO_VICARSTOWN = 25;
+export const VICARSTOWN_TO_TIDMOUTH = 25;
