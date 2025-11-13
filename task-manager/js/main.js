83baad69ea48d823920858d8490e6490b00442ad
Vespertino <Vespertino@C04PC07.informatica.comercio>
2025-11-13 19:56:29 +0100

    su puta madre del false

--- task-manager/js/main.js
+++ task-manager/js/main.js
@@ -3,10 +3,12 @@
 import { Task } from "./models/task.js";
 import { HISTORIC_KEY } from "../js/models/constants.js";
 
-const buttonCrearTarea = document.getElementById("crearTarea");
 let arrayHistorico = [];
 
 loadHistoric();
+
+const buttonCrearTarea = document.getElementById("crearTarea");
+const buttonFiltro = document.getElementById("botonFiltro");
 
 buttonCrearTarea.addEventListener("click", (event) => {
   event.preventDefault;
@@ -15,6 +17,12 @@ buttonCrearTarea.addEventListener("click", (event) => {
   const prioridad = document.getElementById("prioridad").value;
   const tarea = crearTarea(titulo, descripcion, prioridad);
   addHistoricTarea(tarea);
+});
+
+buttonFiltro.addEventListener("click", (event) => {
+  event.preventDefault;
+  const prioridad = document.getElementById("filtroPrioridad").value;
+  filtrarLista(prioridad);
 });
 
 function crearTarea(titulo, descripcion, prioridad, createAt) {
@@ -30,41 +38,101 @@ function mostrarTarea(tarea) {
 
   const botonBorrar = document.createElement("button");
   const textoBotonBorrar = document.createTextNode("Borrar");
+  const botonModificar = document.createElement("button");
+  const textoBotonModificar = document.createTextNode("Modificar");
   const nombreTarea = document.createElement("p");
   const textoNombreTarea = document.createTextNode(tarea.title);
   const descripcionTarea = document.createElement("p");
   const textoDescripcionTarea = document.createTextNode(tarea.description);
   const prioridadTarea = document.createElement("p");
   const textoPrioridadTarea = document.createTextNode(tarea.priority);
+  const doneTarea = document.createElement("p");
+  let textoDoneTarea = document.createTextNode(tarea.done);
+  console.log(textoDoneTarea);
 
   botonBorrar.addEventListener("click", (event) => {
-    borrarTarea(tarea.id);
+    borrarTarea(tarea.title);
+  });
+
+  botonModificar.addEventListener("click", (event) => {
+    completarTarea(tarea.title);
   });
 
   nombreTarea.appendChild(textoNombreTarea);
   descripcionTarea.appendChild(textoDescripcionTarea);
   prioridadTarea.appendChild(textoPrioridadTarea);
+  doneTarea.appendChild(textoDoneTarea);
   botonBorrar.appendChild(textoBotonBorrar);
+  botonModificar.appendChild(textoBotonModificar);
 
   divListaTareas.appendChild(nombreTarea);
   divListaTareas.appendChild(descripcionTarea);
   divListaTareas.appendChild(prioridadTarea);
+  divListaTareas.appendChild(doneTarea);
   divListaTareas.appendChild(botonBorrar);
+  divListaTareas.appendChild(botonModificar);
 
   sectionListaTareas.appendChild(divListaTareas);
 }
 
-function borrarTarea(id) {
-  const resul = arrayHistorico.filter((tarea) => tarea.id == id);
-  return resul;
+function borrarTarea(title) {
+  const idx = arrayHistorico.findIndex((t) => t.title === title);
+  if (idx === -1) return false;
+  arrayHistorico.splice(idx, 1);
+  guardarHistoricTarea(arrayHistorico);
+  recargarDomListas();
 }
-function addHistoricTarea(objTren) {
-  arrayHistorico.push(objTren);
+
+function completarTarea(title) {
+  const idx = arrayHistorico.findIndex((t) => t.title === title);
+  const tarea = arrayHistorico.filter((t) => t.title === title);
+  tarea.done = true;
+
+  if (idx === -1) return false;
+
+  arrayHistorico[idx] = { ...arrayHistorico[idx], ...tarea };
+  guardarHistoricTarea(arrayHistorico);
+  recargarDomListas();
+}
+
+function addHistoricTarea(objTarea) {
+  arrayHistorico.push(objTarea);
   localStorage.setItem(HISTORIC_KEY, JSON.stringify(arrayHistorico));
 }
 
 function guardarHistoricTarea(arrayHistorico) {
   localStorage.setItem(HISTORIC_KEY, JSON.stringify(arrayHistorico));
+}
+
+function recargarDomListas() {
+  const section = document.getElementById("listaTareas");
+  section.innerHTML = "";
+  loadHistoric();
+}
+
+function filtrarLista(prioridad) {
+  const section = document.getElementById("listaFiltroTareas");
+  section.innerHTML = "";
+  const lista = arrayHistorico.filter((t) => t.priority === prioridad);
+  const divListaTareas = document.createElement("div");
+
+  lista.forEach((element) => {
+    const nombreTarea = document.createElement("p");
+    const textoNombreTarea = document.createTextNode(element.title);
+    const descripcionTarea = document.createElement("p");
+    const textoDescripcionTarea = document.createTextNode(element.description);
+    const prioridadTarea = document.createElement("p");
+    const textoPrioridadTarea = document.createTextNode(element.priority);
+
+    nombreTarea.appendChild(textoNombreTarea);
+    descripcionTarea.appendChild(textoDescripcionTarea);
+    prioridadTarea.appendChild(textoPrioridadTarea);
+
+    divListaTareas.appendChild(nombreTarea);
+    divListaTareas.appendChild(descripcionTarea);
+    divListaTareas.appendChild(prioridadTarea);
+  });
+  section.appendChild(divListaTareas);
 }
 
 function loadHistoric() {
@@ -78,7 +146,8 @@ function loadHistoric() {
         element.description,
         element.priority,
         element.createAt,
-        element.id
+        element.id,
+        element.done
       );
     }
   }
