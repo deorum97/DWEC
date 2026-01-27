17e6f23049973d221ecfad679ca40a1618589673
deorum97 <jorge.r.9763@gmail.com>
2026-01-27 14:05:41 +0100

    empezando angular

---
+++ unidad_7/angular/web/src/app/components/housing-location/housing-location.ts
@@ -0,0 +1,12 @@
+import { Component, input } from "@angular/core";
+import { HousingLocationInfo } from "src/app/interfaces/housing-location";
+
+@Component({
+  selector: "app-housing-location",
+  imports: [],
+  templateUrl: `./housing-location.html`,
+  styleUrls: ["./housing-location.css"],
+})
+export class HousingLocation {
+  housingLocation = input.required<HousingLocationInfo>();
+}
